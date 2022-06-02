11060e26d0f0f6ea94e502ed68af4f5a1efa125e
Écio Ferraz <ecioferraz@hotmail.com>
2022-06-02 20:55:09 -0300

    feat: post mongoose schema

---
+++ backend/src/Schema/PostSchema.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+module.exports = new mongoose.Schema({
+  name: String,
+  size: Number,
+  key: String,
+  url: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
