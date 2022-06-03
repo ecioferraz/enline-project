1098af05eb3c028a623abf4f1bb72bbfd7e970bc
Écio Ferraz <ecioferraz@hotmail.com>
2022-06-03 15:09:37 -0300

    refactor: dotenv config

--- backend/src/config/multerConfig.js
+++ backend/src/config/multerConfig.js
@@ -21,7 +21,7 @@ const storageTypes = {
   }),
   s3: multerS3({
     s3: new aws.S3(),
-    bucket: 'enlineuploadchallenge',
+    bucket: process.env.BUCKET_NAME,
     contentType: multerS3.AUTO_CONTENT_TYPE,
     acl: 'public-read',
     key: (req, file, callback) => {
@@ -38,7 +38,7 @@ const storageTypes = {
 
 module.exports = {
   dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-  storage: storageTypes['s3'],
+  storage: storageTypes[process.env.STORAGE_TYPE],
   limits: { fileSize: 2 * 1024 * 1024 },
   fileFilter: (req, file, callback) => {
     const allowedMimes = [
