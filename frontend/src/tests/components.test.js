b89ef3538bfb39b4fedcb7fe64ba3db05436157e
Écio Ferraz <ecioferraz@hotmail.com>
2022-06-03 23:42:32 -0300

    test: file input

--- frontend/src/tests/components.test.js
+++ frontend/src/tests/components.test.js
@@ -45,9 +45,9 @@ describe('Components', () => {
     });
 
     it('should render button\'s properties correctly', () => {
+      expect(button.getAttribute('type')).toBe('button');
       expect(button.getAttribute('name')).toBe('test');
       expect(button.getAttribute('class')).toBe('test');
-      expect(button.getAttribute('type')).toBe('button');
     });
 
     it('should render button\'s inner text correctly', () => {
@@ -68,6 +68,52 @@ describe('Components', () => {
       expect(button).toBeDisabled();
 
       act(() => button.click());
+
+      expect(onClick).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('<FileInput />', () => {
+    let input;
+
+    const onChange = jest.fn();
+
+    beforeEach(async () => {
+      act(() => {
+        render(
+          <FileInput
+            type="file"
+            className="test"
+            labelText="test"
+            value="test"
+            name="test"
+            id="test"
+            handleClick={ onChange }
+          />,
+          container,
+        );
+      });
+
+      input = container.querySelector('input');
+    });
+
+    afterEach(() => input = null);
+
+    it('should render an input', () => {
+      expect(input).toBeTruthy();
+    });
+
+    it('should render file input\'s properties correctly', () => {
+      expect(input.getAttribute('type')).toBe('file');
+      expect(input.getAttribute('class')).toBe('test');
+      expect(input.getAttribute('label')).toBe('test');
+      expect(input.getAttribute('id')).toBe('test');
+      expect(input.getAttribute('value')).toBe('test');
+      expect(input.getAttribute('htmlFor')).toBe('test');
+    });
+
+    it('should render a clickable file input', () => {
+      act(() => input.click());
 
       expect(onClick).not.toHaveBeenCalled();
     });
