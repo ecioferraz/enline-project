91dbd92ab4e646e7724acccadac204b47ad364d2
Écio Ferraz <ecioferraz@hotmail.com>
2022-06-04 15:05:30 -0300

    feat: UploadCard delete upload

--- frontend/src/templates/UploadCard.jsx
+++ frontend/src/templates/UploadCard.jsx
@@ -1,9 +1,21 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Button, ImageCard, TextCard } from '../components';
 import { formatBytes } from '../services/formatBytes';
+import { deleteRequest } from '../services/APIRequests';
+import UploadContext from '../context/UploadContext';
 
-export default function UploadCard({ url, name, size }) {
+export default function UploadCard({ id, url, name, size }) {
+  const [buttonName, setButtonName] = useState('Excluir');
+  const { setUploads } = useContext(UploadContext);
+
+  const removeUpload = async () => {
+    setButtonName('Excluindo...');
+    await deleteRequest(`/${id}`);
+    setUploads((prevState) => prevState.filter((upload) => upload._id !== id));
+    setButtonName('Excluir');
+  };
+
   return (
     <div>
       <ImageCard
@@ -19,15 +31,17 @@ export default function UploadCard({ url, name, size }) {
         className="upload-size"
       />
       <Button
-        name="Excluir"
-        handleClick={ () => {} }
+        name={ buttonName }
+        handleClick={ removeUpload }
         className="remove-btn"
+        disabled={ buttonName === 'Excluindo...' }
       />
     </div>
   );
 }
 
 UploadCard.propTypes = {
+  id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   size: PropTypes.number.isRequired,
   url: PropTypes.string.isRequired
