7ef087395e7847548f77fa859a1aab6ad38bb1db
Écio Ferraz <ecioferraz@hotmail.com>
2022-06-04 13:06:36 -0300

    feat: ImageCard component

---
+++ frontend/src/components/ImageCard.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default function ImageCard({ src, title }) {
+  return (
+    <img
+      src={ src }
+      alt={ `Imagem de nome: ${title}` }
+      className="uploaded-img"
+    />
+  );
+}
+
+ImageCard.propTypes = {
+  src: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
