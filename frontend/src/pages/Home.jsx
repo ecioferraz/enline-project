def8688c1000321259b8aca759ea1fc6a1075408
Écio Ferraz <ecioferraz@hotmail.com>
2022-06-04 11:36:56 -0300

    feat: FileForm route

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FileForm from '../templates/FileForm';
+
+export default function Home() {
+  return (
+    <main>
+      <FileForm />
+    </main>
+  );
+}
--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home';
 
 export default function Router() {
   return (
     <Routes>
-      <Route />
+      <Route path="/" element={ <Home /> } />
     </Routes>
   );
 }
