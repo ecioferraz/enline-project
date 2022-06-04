dc07f2be88dea6dcac46bb736cc008aa98673654
Écio Ferraz <ecioferraz@hotmail.com>
2022-06-04 18:55:43 -0300

    procfile

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,9 +17,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-app.use('/files', express.static(
-  path.resolve(__dirname, '..', 'tmp', 'uploads')));
+app.use(
+  '/files',
+  express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+);
 
 app.use(router);
 
-app.listen(3001);
+// app.use(express.static(path.join(__dirname + '../frontend/build')));
+// app.get('*', (req, res) => res.sendFile(
+//   path.join(__dirname, '../frontend/build'),
+// ));
+
+app.listen(process.env.PORT || 3001);
